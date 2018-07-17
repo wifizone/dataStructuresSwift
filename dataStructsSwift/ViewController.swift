5ae12837ce5cee2ad781275e85c9aed7f3e0cc48
Anton Poluianov <>
2018-07-17 14:36:20 +0300

    Initial

---
+++ dataStructsSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dataStructsSwift
+//
+//  Created by Anton Poluianov on 17/07/2018.
+//  Copyright © 2018 Anton Poluianov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
